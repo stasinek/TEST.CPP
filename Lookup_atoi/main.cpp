593fb495119ad974dd041f79e17dcbdab2a5c59e
stasinek <sstsoft@wp.pl>
2023-06-17 13:48:49 +0200

    FreeBSD Apple added to comparison,  stdlib of your complier, Apple, Lookup Plus improved formating of output, if result of atoi = 0 then lookup is not considered error automatically because it does support more formats

--- Lookup_atoi/main.cpp
+++ Lookup_atoi/main.cpp
@@ -1,6 +1,11 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <limits.h>
+#include <ctype.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <memory.h>
+//#include <sys/cdefs.h>
 // -----------------------------------------------------------------------------
 
 	const int table_bin[32][2] = {
@@ -64,14 +69,152 @@
 // -----------------------------------------------------------------------------
 /*
 How about basic idea?
+copy paste whatever you want to compare with!
 */
 // -----------------------------------------------------------------------------
 
 int atoi_reference(const char *astr)
 {
-// maybe lated :D
-// copy paste whatever you want to compare with!
-return atoi(astr);
+/*-
+    FreeBSD - strtol (str to long int)
+
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+#if defined(LIBC_SCCS) && !defined(lint)
+static char sccsid[] = "@(#)strtol.c	8.1 (Berkeley) 6/4/93";
+#endif /* LIBC_SCCS and not lint */
+//__FBSDID("$FreeBSD: src/lib/libc/stdlib/strtol.c,v 1.20 2007/01/09 00:28:10 imp Exp $");
+/*
+ * Convert a string to a long integer.
+ *
+ * Assumes that the upper and lower case
+ * alphabets and digits are each contiguous.
+ */
+#ifdef __BORLANDC__
+#define __restrict
+#define isspace_l(c,l) c == ' '  // filter out white characters, could be SPACE alone or any charter other than 0-9,A-F
+#else
+locale_t loc;
+	NORMALIZE_LOCALE(loc);
+#endif
+
+    const char * __restrict nptr = astr;
+    char ** __restrict endptr = NULL;        // return ERROR - atoi does not support that
+    int base = 10;
+	const char *s;
+	unsigned long acc;
+	char c;
+	unsigned long cutoff;
+	int neg, any, cutlim;
+
+	/*
+	 * Skip white space and pick up leading +/- sign if any.
+	 * If base is 0, allow 0x for hex and 0 for octal, else
+	 * assume decimal; if base is already 16, allow 0x.
+	 */
+	s = nptr;
+	do {
+		c = *s++;
+	} while (isspace_l((unsigned char)c, loc));
+	if (c == '-') {
+		neg = 1;
+		c = *s++;
+	} else {
+		neg = 0;
+		if (c == '+')
+			c = *s++;
+	}
+	if ((base == 0 || base == 16) &&
+	    c == '0' && (*s == 'x' || *s == 'X') &&
+	    ((s[1] >= '0' && s[1] <= '9') ||
+	    (s[1] >= 'A' && s[1] <= 'F') ||
+	    (s[1] >= 'a' && s[1] <= 'f'))) {
+		c = s[1];
+		s += 2;
+		base = 16;
+	}
+	if (base == 0)
+		base = c == '0' ? 8 : 10;
+	acc = any = 0;
+	if (base < 2 || base > 36)
+		goto noconv;
+
+	/*
+	 * Compute the cutoff value between legal numbers and illegal
+	 * numbers.  That is the largest legal value, divided by the
+	 * base.  An input number that is greater than this value, if
+	 * followed by a legal input character, is too big.  One that
+	 * is equal to this value may be valid or not; the limit
+	 * between valid and invalid numbers is then based on the last
+	 * digit.  For instance, if the range for longs is
+	 * [-2147483648..2147483647] and the input base is 10,
+	 * cutoff will be set to 214748364 and cutlim to either
+	 * 7 (neg==0) or 8 (neg==1), meaning that if we have accumulated
+	 * a value > 214748364, or equal but the next digit is > 7 (or 8),
+	 * the number is too big, and we will return a range error.
+	 *
+	 * Set 'any' if any `digits' consumed; make it negative to indicate
+	 * overflow.
+	 */
+	cutoff = neg ? (unsigned long)-(LONG_MIN + LONG_MAX) + LONG_MAX
+	    : LONG_MAX;
+	cutlim = cutoff % base;
+	cutoff /= base;
+	for ( ; ; c = *s++) {
+		if (c >= '0' && c <= '9')
+			c -= '0';
+		else if (c >= 'A' && c <= 'Z')
+			c -= 'A' - 10;
+		else if (c >= 'a' && c <= 'z')
+			c -= 'a' - 10;
+		else
+			break;
+		if (c >= base)
+			break;
+		if (any < 0 || acc > cutoff || (acc == cutoff && c > cutlim))
+			any = -1;
+		else {
+			any = 1;
+			acc *= base;
+			acc += c;
+		}
+	}
+	if (any < 0) {
+		acc = neg ? LONG_MIN : LONG_MAX;
+		errno = ERANGE;
+	} else if (!any) {
+noconv:
+		errno = EINVAL;
+	} else if (neg)
+		acc = -acc;
+	if (endptr != NULL)
+		*endptr = (char *)(any ? s - 1 : nptr);
+	return (acc);
 }
 // -----------------------------------------------------------------------------
 /*
@@ -81,21 +224,26 @@ How about NOT using multiplication? But lookup tables?
 
 int atoi_lookup(const char *astr)
 {
-// on 32bit platform limit is 1 sign, 2 for hex,bin notation: 0x,0b + 8,10,32 digits = 11-34
-// but if you want filter out, trim mush allow higher limit
 #define MY_POS_LIMIT 100
+/* on 32bit platform limit is: 1 byte for sign, 2 bytes for hex,bin notation: 0x,0b 
+   and + HEX 8 or DEC 10 or BIN 32 digits = 11-34 bytes string 
+ if you want filter out more, trim mush allow higher limit or even no limits */
+
 // on 32bit platform parameters are send via stack,
-//this allow str to be put into register!
+// this allow str to be put into register
 const char *str = astr;
-// 3 lines of code, decission making purpose, allow to pick table of choice dec,hex,bin
+
+// determine valid ranges of characters 0-9, A-F
 const char base_bin_ascii = '0';
 const char last_bin_ascii = '1';
-const char base_dec_ascii = '0'; // both for HEX and DEC! 48='0'
-const char last_dec_ascii = '9'; // both for HEX and DEC! 58='9'
-const char base_hex_ascii = 'A'; // 65='A'
-const char last_hex_ascii = 'F'; // 70='F'
-//const char base_hex_ascii = 'a'; // 97='a'
-//const char last_hex_ascii = 'f'; // 102='f'
+const char base_dec_ascii = '0'; // valid both for HEX and DEC! 48='0'
+const char last_dec_ascii = '9'; // valid both for HEX and DEC! 58='9'
+const char base_hex_ascii = 'A'; // 65 = 'A'
+const char last_hex_ascii = 'F'; // 70 = 'F'
+/*
+ const char base_hex_ascii = 'a'; // 97 = 'a'
+ const char last_hex_ascii = 'f'; // 102 = 'f'
+*/
 // we could mask bit nr 5 (32) to make upper letters so one range is enough!
 // also x and b will turn into -> X and B
 
@@ -382,18 +530,28 @@ Speed test
 
 int main(int argc, char** argv) {
 
-		int TEST_stdlib_result, TEST_my_result;
+		int TEST_stdlib_result, TEST_reference_result, TEST_my_result;
 		char *TEST_str,TEST_itoabuff[20];
+        char TEST_br[81];
+        memset(TEST_br, '-', 78);
+        TEST_br[78] = '\n';
+        TEST_br[79] = '\r';
+        TEST_br[80] = '\0';
 
-		printf("TEXT\t\t\tATOI\t\tMY(DEC)\t\tMY(HEX)\n\r");
 
-		#define TEST(__ATEST_STR)                                       \
-		TEST_str = __ATEST_STR;                                         \
-		TEST_my_result = atoi_lookup(TEST_str);                         \
-                TEST_stdlib_result = atoi_reference(TEST_str);                  \
-		printf("%12s\t%11d\t%11d\t%08x\t",TEST_str,TEST_stdlib_result, TEST_my_result, TEST_my_result); \
-		if (TEST_stdlib_result==TEST_my_result) printf(" - OK\n\r");    \
-		else printf(" - ERROR\n\r");
+		printf("TEXT\t\t\tATOI\t\tREF(DEC)\tMY(DEC)\t\tMY(HEX)\n\r");
+
+		#define TEST(__ATEST_STR)                                               \
+		TEST_str = __ATEST_STR;                                                 \
+		TEST_my_result = atoi_lookup(TEST_str);                                 \
+        TEST_reference_result = atoi_reference(TEST_str);                       \
+        TEST_stdlib_result = atoi(TEST_str);                                    \
+        printf("%s",TEST_br);                                                   \
+        printf("%12s\t%11d\t%11d\t%11d\t0x%08x\n\r",                            \
+                TEST_str, TEST_stdlib_result, TEST_reference_result, TEST_my_result, TEST_my_result);       \
+		if (TEST_stdlib_result==TEST_my_result) printf("^^ OK: CORRECT 100%%\n\r");                         \
+        else if (TEST_stdlib_result==0) printf("^^ WARNING: NOT SUPPORTED BY ATOI, CHECK MANUALLY\n\r");    \
+		        else printf("^^ ERROR: ABSOLUTELY! \n\r");
 
 		TEST("0x24549ABC")
 		TEST("0b10")
@@ -415,16 +573,23 @@ int main(int argc, char** argv) {
                 int i;
 
                 #define TEST_SPEED(TEST_NAME,TEST_FUNC)                         \
-                printf("TESTING SPEED %s..\n\r",TEST_NAME);                     \
+                printf("\n\r");                                                 \
+                printf("TESTING SPEED %s.. ",TEST_NAME);                     \
                 old_time = time(NULL);                                          \
                 for (runda = 0; runda < 10000000; runda++)                      \
                 for (i = 0; i < 10; i++) TEST_FUNC;                             \
                 end_time = time(NULL);                                          \
                 printf("%s time = %d\n\r",TEST_NAME,end_time-old_time);
 
-                TEST_SPEED("STDLIB",atoi(testy[i]);)
+                TEST_SPEED("Compiler STDLIB",atoi(testy[i]);)
+                    int stdlib_time = end_time-old_time;
+                TEST_SPEED("Apple FreeBSD",atoi_reference(testy[i]);)
+                    int reference_time = end_time-old_time;
                 TEST_SPEED("LOOKUP",atoi_lookup(testy[i]);)
-                //TEST_SPEED("REFERENCE",atoi_reference(testy[i]);)
+                    int my_time = end_time-old_time;
+
+                printf("Relative speed, lower is faster, STDLIB=100%% APPLE=%d%% LOOKUP=%d%%",
+                            (reference_time*100)/stdlib_time,(my_time*100)/stdlib_time);
                 system("pause");
 
 	return 0;
